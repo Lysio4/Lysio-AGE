619af4956b38616d33b21f289655634ab71852ee
Lysio4 <lukas.moncorge@proton.me>
2026-04-20 12:42:08 +0200

    forgot to remove something

--- config/formats.ts
+++ config/formats.ts
@@ -31,7 +31,6 @@ After that, find the right section within the formats list that describes your m
 
 // Imports
 import { Formats as Abilitypos                  } from '../data/mods/abilitypos/formats';
-import { Formats as AGoldenExperience           } from '../data/mods/agoldenexperience/formats';
 import { Formats as Alternatium                 } from '../data/mods/alternatium/formats';
 import { Formats as AlternatiumEX               } from '../data/mods/alternatiumex/formats';
 import { Formats as BackToSinnoh                } from '../data/mods/backtosinnoh/formats';
